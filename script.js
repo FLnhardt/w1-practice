987a1c814cc1c96e1edd50d1b3bc9f85e8af54bf
fnlinhrdt <fannilinhardt@gmail.com>
2023-09-28 10:45:39 +0200

    done

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function logger(param) {
-    console.log(param)
-  }
-  
-  logger("kicsmacska") // FÜGGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
-  
-  function sumOfTwoNumbers(number1, number2) {
-    //console.log(number1 + number2)
-    let sum = number1 + number2
-    return sum // ITT TÉR VISSZA A FÜGGVÉNY AZ EREDMÉNNYEL
-  }
-  
-  /* let result = sumOfTwoNumbers(2, 4)
-  logger(result) */
-  
-  logger(sumOfTwoNumbers(5, 10))
+let variable1 = 'kismacska' //string
+let variable2 = 101 //number
+let variable3 = true //boolean
+let variable4 = null //null
+
+let variableArrey = ["alma", "barack", "szilva", "vegyes"] //tombok
+let variableArray2 = [1, 2, 3, 4, 5]
+let variableArrey3 = ["auto", 54, true, undefined, "62"]
+let variableArrey4 = [variable1, variable2, variable3, variable4]
+ //nagyon fontos a tombben milyen sorrendben vannak az ertekek [indexszamok]mindig a 0-s indextol kezdodik a tombunk
+console.log(variableArrey[0])
+
+let counter = 0 
+
+while (counter < 5) {
+  console.log(counter)
+  counter = counter +1 //egyszerubb a counter ++ egyel noveli az erteket, counter -- egyel csokkenti a erteket
+}
+
+for (let counter = 0; counter <5;counter++) {
+  console.log(counter)
+}
+//for ciklust gyakorolni, FONTOS!!
+//MINDEN STRING KEZELHETO TOMBBKENT 
+
---
+++ script2.js
@@ -0,0 +1,17 @@
+function animal(param) {
+  console.log(param);
+}
+
+animal("dog");
+
+function sumOfThreeNumbers(number1, number2, number3) {
+  let result = number1 * number2 * number3;
+  return result;
+}
+
+let result = sumOfThreeNumbers(3, 4, 5);
+animal(result);
+
+let myName = "Fanni"
+let herName = 'Kriszti'
+console.log (myName , herName)
